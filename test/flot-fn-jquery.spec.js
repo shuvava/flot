f7254b464542a90ac562cab2b50f17c167825138
geek <keeper2002@mail.ru>
2017-12-23 08:58:53 -0800

    added test

--- test/flot-fn-jquery.spec.js
+++ test/flot-fn-jquery.spec.js
@@ -1,16 +1,30 @@
 /* globals describe it assert */
 // import { assert } from 'chai';
 import $ from 'jquery';
-import { appendTo } from '../src/flot-fn-jquery';
+import { appendTo, setStyle } from '../src/flot-fn-jquery';
+
+const _module_ = 'flot-fn-jquery';
 
 describe('flot-fn-jquery', () => {
     describe('#appendTo()', () => {
         it('should be not null', () => {
+            const id = `${_module_}-appendTo`;
             const body = $('body');
-            const elm = $('<div id="flot-fn-jquery-appendTo"></div>');
+            const elm = $(`<div id="${id}"></div>`);
             appendTo(body, elm);
-            const elms = document.getElementById('flot-fn-jquery-appendTo');
+            const elms = document.getElementById(id);
             assert.isNotNull(elms.length);
+        });
+    });
+    describe('#setStyle()', () => {
+        it('should have style', () => {
+            const id = `${_module_}-setStyle`;
+            const body = $('body');
+            const elm = $(`<div id="${id}"></div>`);
+            appendTo(body, elm);
+            setStyle(elm, { position: 'absolute' });
+            const elmDOM = document.getElementById(id);
+            assert.equal(elmDOM.style.position, 'absolute');
         });
     });
 });
